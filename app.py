50db761a77ebdc47a43d3f6ced3a112d072f2054
Yuri Rondan <149972773+Yusurus@users.noreply.github.com>
2026-03-09 21:50:16 -0500

    Eliminar ruta de prueba de Cloudinary y agregar archivos de configuración de requisitos y Vercel

--- app.py
+++ app.py
@@ -14,23 +14,6 @@ app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
 app.permanent_session_lifetime = timedelta(days=7)
 
-# --- RUTA DE PRUEBA DE CLOUDINARY ---
-@app.route('/test-cloudinary')
-def test_cloudinary():
-    # 1. Primero, revisamos qué está leyendo Python de tu .env
-    cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME')
-    api_key = os.getenv('CLOUDINARY_API_KEY')
-    
-    html_debug = f"<h3>Datos leídos del .env:</h3>"
-    html_debug += f"<ul><li>Cloud Name: {cloud_name}</li><li>API Key: {api_key}</li></ul>"
-    
-    # 2. Intentamos hacer "Ping" a los servidores
-    try:
-        respuesta = cloudinary.api.ping()
-        return html_debug + f"<h3 style='color:green;'>✅ Conexión exitosa a Cloudinary:</h3> <p>{respuesta}</p>"
-    except Exception as e:
-        return html_debug + f"<h3 style='color:red;'>❌ Error de conexión:</h3> <p>{str(e)}</p>"
-
 # --- CONFIGURACIÓN DE CLOUDINARY ---
 cloudinary.config(
   cloud_name = os.getenv('CLOUDINARY_CLOUD_NAME'),
